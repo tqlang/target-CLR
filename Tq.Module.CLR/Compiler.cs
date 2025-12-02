a6dabb8c89fb3217c2ddbe80e10ae1ec96c2ba9a
Camila <94462076+lumi2021@users.noreply.github.com>
2025-12-02 00:17:24 -0300

    first commit

---
+++ Tq.Module.CLR/Compiler.cs
@@ -0,0 +1,16 @@
+using Tq.Realizeer.Core.Program;
+using Tq.Realizer.Core.Configuration.LangOutput;
+
+namespace Tq.Module.CLR;
+
+public static class Compiler
+{
+
+    public static void Compile(RealizerProgram program, IOutputConfiguration config)
+    {
+        Console.WriteLine("Dong shit");
+        
+        
+    }
+    
+}
